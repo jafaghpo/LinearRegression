4fac2aa281e488470f5e6826af195af4da323bb6
jafaghpo <jafaghpo@student.42.fr>
2018-04-17 01:26:48 +0200

    [ADD] estimation part
    initialization of repository + estimate.py

---
+++ dataset.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+__version__ = '1.0'
+__author__ = 'John Afaghpour'
+
+"""
+    Tools functions to manage datasets.
+"""
+
+from pandas import read_csv
+
+
+def normalize(x, min, max):
+
+    return (x - min) / (max - min)
+
+
+def denormalize(x, min, max):
+
+    return x * (max - min) + min
+
+
+def get_training(path):
+
+    try:
+        data = read_csv(path, dtype='float')
+    except Exception as e:
+        exit("error: {}".format(e))
+    return data
+
+
+def get_theta(path):
+
+    try:
+        data = read_csv(path, dtype='float')
+        theta = [float(data.t0), float(data.t1)]
+    except Exception:
+        theta = [0, 0]
+    return theta
+
+
+if __name__ == '__main__':
+    print('This is a module containing only functions so there is no point in running this file.')
---
+++ estimate.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+__version__ = '1.0'
+__author__ = 'John Afaghpour'
+
+"""
+    Estimates the price of a car based on mileage,
+    using thetas value (weight for training) stored
+    in theta.csv. Values are previously normalized to
+    optimize training speed.
+"""
+
+import dataset
+
+
+def estimate_price(data, theta):
+
+    try:
+        mileage = input("Please enter a mileage to estimate the price: ")
+        mileage = float(mileage)
+    except (TypeError, NameError) as e:
+        exit('error: mileage is not valid')
+    except (KeyboardInterrupt, SystemExit):
+        exit('\nInterrupted')
+    if mileage < 0:
+        print('A mileage cannot be negative, try again')
+        return estimate_price(data, theta)
+    try:
+        mileage = dataset.normalize(mileage, min(data.km), max(data.km))
+    except ZeroDivisionError as e:
+        exit('error: {}'.format(e))
+    price = theta[0] + theta[1] * mileage
+    if price > 0:
+        price = dataset.denormalize(price, min(data.price), max(data.price))
+    return price if price > 0 else 0
+
+
+if __name__ == '__main__':
+    data = dataset.get_training('data.csv')
+    theta = dataset.get_theta('theta.csv')
+    price = estimate_price(data, theta)
+    print('Estimated price is {}$'.format(price))
